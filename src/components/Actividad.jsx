83d34ea6da882e0848aa9fbb0f1f9656395c1a52
Pepo2405 <ignacioniglesias@gmail.com>
2022-12-12 00:27:15 -0300

    fotos

--- src/components/Actividad.jsx
+++ src/components/Actividad.jsx
@@ -4,18 +4,14 @@ import { EditIcon } from "../assets/icons";
 import styles from "../styles/ActivityCard.module.css";
 
 export const Actividad = ({ name, dificulty, season, duration, id, img }) => {
+  const imagen =
+    img ||
+    "https://res.cloudinary.com/onlypepo/image/upload/v1670799257/proyectoIndividual/v82kluagq4w2jrpnb8pn.png";
   return (
     <div className={styles.actividad} key={name}>
       <div className={styles.texto}>
         <p className={styles.actTitulo}>{name}</p>
-        <img
-          src={
-            img
-              ? img
-              : "https://res.cloudinary.com/onlypepo/image/upload/v1670799257/proyectoIndividual/v82kluagq4w2jrpnb8pn.png"
-          }
-          style={{ maxWidth: "200px", maxHeight: "150px" }}
-        />
+        <img src={imagen} style={{ maxWidth: "200px", maxHeight: "150px" }} />
         <p>Difficultad : {dificulty}</p>
         <p>Duracion :{duration}</p>
         <p>Temporada: {season}</p>
